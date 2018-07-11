718a5290c40d8ef1f9b52f902765d8b5c4713643
GermantasSt <germantas.stasytis@gmail.com>
2018-07-11 21:26:16 +0300

    Project template

--- Guest List/guestlist.js
+++ Guest List/guestlist.js
@@ -15,43 +15,45 @@ const buttons = document.querySelectorAll("button");
 const arrayGuest = [];
 guestList.innerHTML = arrayGuest;
 
-function replace(){
-    let oldGuestIndex = arrayGuest.findIndex(value => value === `${oldGuest.value}<br/>`);
-    if (oldGuestIndex !== -1) {
-        arrayGuest[oldGuestIndex] = newGuest.value;
-    } else {
-        alert("This Guest is not on the list");
-    }
+function replace() {
+  let oldGuestIndex = arrayGuest.findIndex(
+    value => value === `${oldGuest.value}<br/>`
+  );
+  if (oldGuestIndex !== -1) {
+    arrayGuest[oldGuestIndex] = newGuest.value;
+  } else {
+    alert("This Guest is not on the list");
+  }
 }
 
-function manageGuest(event){
-    const clickedButton = event.target.id;
-    switch(clickedButton){
-        case "addGuestEnd":
-            arrayGuest.push(guestInput.value + "<br/>");
-            break;
-        case "addGuestStart":
-            arrayGuest.unshift(guestInput.value + "<br/>");
-            break;
-        case "removeFirst":
-            arrayGuest.shift(guestInput.value + "<br/>");
-            break;
-        case "removeLast":
-            arrayGuest.pop(guestInput.value + "<br/>");
-            break;
-        case "reverseList":
-            arrayGuest.reverse();
-            break;
-        case "sortAZ":
-            arrayGuest.sort();
-            break;
-        case "sortZA":
-            arrayGuest.sort((a,b) => a < b);
-            break;
-        case "replaceGuest":
-            replace();
-            break;
-    }
-    guestList.innerHTML = arrayGuest.join('');
+function manageGuest(event) {
+  const clickedButton = event.target.id;
+  switch (clickedButton) {
+    case "addGuestEnd":
+      arrayGuest.push(guestInput.value + "<br/>");
+      break;
+    case "addGuestStart":
+      arrayGuest.unshift(guestInput.value + "<br/>");
+      break;
+    case "removeFirst":
+      arrayGuest.shift(guestInput.value + "<br/>");
+      break;
+    case "removeLast":
+      arrayGuest.pop(guestInput.value + "<br/>");
+      break;
+    case "reverseList":
+      arrayGuest.reverse();
+      break;
+    case "sortAZ":
+      arrayGuest.sort();
+      break;
+    case "sortZA":
+      arrayGuest.sort((a, b) => a < b);
+      break;
+    case "replaceGuest":
+      replace();
+      break;
+  }
+  guestList.innerHTML = arrayGuest.join("");
 }
 buttons.forEach(button => button.addEventListener("click", manageGuest));
